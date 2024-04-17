48ac4f3ee300693e953ccceaa4ff66639e9386fa
duthd3 <duthd3@naver.com>
2024-04-17 18:35:49 +0900

    [FEAT] 홈화면 UI 구현 및 API 연동. (광고는 API 없음, 배너도 사진 데이터 없어서 임시로 message만 넣어놓은 상태)

---
+++ NanaLand/NanaLand/Home/RecommendModel.swift
@@ -0,0 +1,21 @@
+//
+//  RecommendModel.swift
+//  NanaLand
+//
+//  Created by jun on 4/16/24.
+//
+
+import Foundation
+
+struct RecommendModel: Codable, Hashable {
+    var message: String
+    var data: [Data]
+}
+
+struct Data: Codable, Hashable {
+    var id: Int
+    var category: String
+    var thumbnailUrl: String
+    var title: String
+    var intro: String
+}
