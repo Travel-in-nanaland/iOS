126924c520cfec8365beba4467e2e93c933d016e
HyunWoo Jeong <jrwedo@naver.com>
2024-04-18 14:59:52 +0900

    [RENAME] Search 폴더 구조 정리

--- NanaLand/NanaLand/Home/Search/Model/SearchDTO.swift
+++ NanaLand/NanaLand/Home/Search/Model/SearchDTO.swift
@@ -1,17 +1,11 @@
 //
-//  Article.swift
+//  SearchDTO.swift
 //  NanaLand
 //
-//  Created by 정현우 on 4/16/24.
+//  Created by 정현우 on 4/18/24.
 //
 
 import Foundation
-
-struct Article: Codable {
-	let id: Int
-	let thumbnailUrl: String
-	let title: String
-}
 
 struct SearchDetailCategoryResponse: Codable {
 	let count: Int
