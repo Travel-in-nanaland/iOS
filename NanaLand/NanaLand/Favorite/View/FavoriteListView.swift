a3a2d37c819a0e91035537a920589ed794fe8e1d
Riu <wodnd0418@gmail.com>
2024-07-14 03:24:10 +0900

    [FIX] MVP1 오류사항 수정

--- NanaLand/NanaLand/Favorite/View/FavoriteListView.swift
+++ NanaLand/NanaLand/Favorite/View/FavoriteListView.swift
@@ -1,77 +1,91 @@
-//
-//  FavoriteListView.swift
-//  NanaLand
-//
-//  Created by 정현우 on 4/24/24.
-//
-
 import SwiftUI
 
 struct FavoriteListView: View {
-	@EnvironmentObject var favoriteVM: FavoriteViewModel
-	let category: Category
-	
-	@State var isInit: Bool = false
-	
+    @EnvironmentObject var favoriteVM: FavoriteViewModel
+    let category: Category
+    
+    @State var isInit: Bool = false
+    
     var body: some View {
-		ScrollView(.vertical) {
-			VStack {
-				LazyVGrid(
-					columns: [GridItem(.flexible()), GridItem(.flexible())]
-				) {
-					ForEach({
-						switch category {
-						case .all:
-							return favoriteVM.state.allFavoriteArticles.data
-						case .nature:
-							return favoriteVM.state.natureFavoriteArticles.data
-						case .festival:
-							return favoriteVM.state.festivalFavoriteArticles.data
-						case .market:
-							return favoriteVM.state.marketFavoriteArticles.data
-						case .experience:
-							return favoriteVM.state.experienceFavoriteArticles.data
-						case .nanaPick:
-							return favoriteVM.state.nanaFavoriteArticles.data
-						}
-					}() as [Article],
-						id: \.self
-					) { article in
-						ArticleItem(category: category, article: article, onTapHeart: {
-							Task {
-								await favoriteVM.action(.deleteItemInFavoriteList(tab: category, article: article))
-							}
-						})
-					}
-					
-					if !favoriteVM.isLastPage(tab: category) {
-						ProgressView()
-							.task {
-								await favoriteVM.action(.getFavoriteList(category: category))
-							}
-							.frame(width: 20)
-							.position(x: Constants.screenWidth/2 - 20)
-					}
-					
-					
-				}
-			}
-		}
-		.padding(.horizontal, 16)
-		.padding(.top, 0)
-		.task {
-			if !isInit {
-				await favoriteVM.action(.getFavoriteList(category: category))
-				isInit = true
-			} else {
-				await favoriteVM.action(.refreshData(category: category))
-			}
-		}
+        ScrollView(.vertical) {
+            VStack {
+                LazyVGrid(
+                    columns: [GridItem(.flexible()), GridItem(.flexible())]
+                ) {
+                    ForEach({
+                        switch category {
+                        case .all:
+                            return favoriteVM.state.allFavoriteArticles.data
+                        case .nature:
+                            return favoriteVM.state.natureFavoriteArticles.data
+                        case .festival:
+                            return favoriteVM.state.festivalFavoriteArticles.data
+                        case .market:
+                            return favoriteVM.state.marketFavoriteArticles.data
+                        case .experience:
+                            return favoriteVM.state.experienceFavoriteArticles.data
+                        case .nanaPick:
+                            return favoriteVM.state.nanaFavoriteArticles.data
+                        }
+                    }() as [Article],
+                        id: \.self
+                    ) { article in
+                        NavigationLink {
+                            destinationView(for: article)
+                        } label: {
+                            ArticleItem(category: category, article: article, onTapHeart: {
+                                Task {
+                                    await favoriteVM.action(.deleteItemInFavoriteList(tab: category, article: article))
+                                }
+                            })
+                        }
+                    }
+                }
+                if !favoriteVM.isLastPage(tab: category) {
+                    ProgressView()
+                        .task {
+                            await favoriteVM.action(.getFavoriteList(category: category))
+                        }
+                        .frame(width: 20)
+                        .position(x: Constants.screenWidth/2 - 20)
+                }
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.top, 0)
+        .task {
+            if !isInit {
+                await favoriteVM.action(.getFavoriteList(category: category))
+                isInit = true
+            } else {
+                await favoriteVM.action(.refreshData(category: category))
+            }
+        }
     }
-	
+    
+    @ViewBuilder
+        func destinationView(for article: Article) -> some View {
+            switch article.category {
+            case .nature:
+                NatureDetailView(id: Int64(article.id))
+            case .festival:
+                FestivalDetailView(id: Int64(article.id))
+            case .market:
+                ShopDetailView(id: Int64(article.id))
+            case .experience:
+                Text("Experience Detail View")
+    //            ExperienceDetailView(id: article.id)
+            case .nanaPick:
+                NaNaPickDetailView(id: Int64(article.id))
+            case .all:
+                Text("test")
+            }
+        }
 }
 
 #Preview {
-	FavoriteListView(category: .all)
-		.environmentObject(FavoriteViewModel())
+    FavoriteListView(category: .all)
+        .environmentObject(FavoriteViewModel())
 }
+
+
