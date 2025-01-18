31c8214daac7b050eed9a843beff5cfc0fb0c154
duthd3 <duthd3@naver.com>
2025-01-18 16:13:01 +0900

    ut 수정 477,478

--- NanaLand/NanaLand/Common/Util/Toast.swift
+++ NanaLand/NanaLand/Common/Util/Toast.swift
@@ -24,7 +24,7 @@ struct Toast: View {
                         .padding()
                         .background(Color.black.opacity(0.7))
                         .foregroundColor(.white)
-                        .multilineTextAlignment(.trailing)
+                        .multilineTextAlignment(.center)
                         .cornerRadius(30)
                         .offset(y: -20)
                         .animation(.easeInOut(duration: 0.5), value: isAnimating)
